4b2c9700f8141037fbaa94c861b92b752efe9063
EdgeWoodruff <55330464+EdgeWoodruff@users.noreply.github.com>
2026-01-22 00:10:18 -0800

    Update Song.java
    I added the rest of the code for the Song.java class.
    The constructors, the getters, and the rest of the methods.

--- Song.java
+++ Song.java
@@ -1,7 +1,7 @@
 /**
  * File for a Song class to be used in the Playlist Project
- * @author
- * @version 
+ * @author Jad and Edge
+ * @version 1
  */
 public class Song {
     //Fields-- what information do we want each Song to store?
@@ -13,18 +13,48 @@ public class Song {
      * Constructor-- what information needs to be given to make a Song?
      * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
      */
+    
+public Song(String title, String artist, int minutes, int seconds) {
+        this.title = title;
+        this.artist = artist;
+        this.durationSeconds = minutes * 60 + seconds;
+        this.liked = false; 
+    }
 
+public String getTitle() {
+        return title;
+    }
+public String getArtist() {
+        return artist;
+    }
+public int getDurationSeconds() {
+        return durationSeconds;
+    }
+public boolean isLiked() {
+        return liked;
+    }
+    
+public void like() {
+        liked = true;
+    }
 
+public void unlike() {
+        liked = false;
+    }
 
-
-
-     /**
-      * Methods-- what will you want each Song to do?
-      * Consider all the getter methods (getName, getArtist, etc.)
-      * You should probably have a toString method to be able to get the information for the full
-      * song easily as well!
-      * What kind of mutator (setter) methods will you need?
-      */
-
+public String getDurationString() {
+        int minutes = durationSeconds / 60;
+        int seconds = durationSeconds % 60;
+        return String.format("%d:%02d", minutes, seconds);
+    }
+    
+public String toString() {
+        String result = "\"" + title + "\" by " + artist + " (" + getDurationString() + ")";
+        if (liked) {
+            result += " -- liked";
+        }
+        return result;
+    }
+}
 
 }
