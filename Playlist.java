308c4c1459cbf315c3faa9e06ee7dd92af6f70f6
EdgeWoodruff <55330464+EdgeWoodruff@users.noreply.github.com>
2026-01-22 00:27:35 -0800

    Update Playlist.java
    Fully completed playlist.java. I wrote it on vscode first. This took a long time hopefully it works. I had to look up some things on the internet to help me.

--- Playlist.java
+++ Playlist.java
@@ -1,34 +1,81 @@
 import java.util.ArrayList;
 
-/**
- * The Playlist class, which will keep track of a playlist of Song objects
- * Refer to the project description to make sure you have access to all available methods
- */
 public class Playlist {
-    /**
-     * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
-     * (CarDealership, Zoo) for structure on how this will look
-     */
+
+    private ArrayList<Song> songs;
 
 
+    public Playlist() {
+        songs = new ArrayList<Song>();
+    }
 
 
-     /**
-      * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
-      * then use additional methods to add Songs in one-by-one
-      */
+    public void addSong(Song s) {
+        songs.add(s);
+        System.out.println("Added " + s.toString());
+    }
 
 
-      
+    public void likeSong(int position) {
+        int loc = position - 1; 
+        if (loc >= 0 && loc < songs.size()) {
+            songs.get(loc).like();
+        } else {
+            System.out.println("Cannot like ts song: position " + position + " out of range. Lock in.");
+        }
+    }
 
-      /**
-       * Methods-- Remember that you need to be able to complete all of the following:
-       * Adding a song
-       * 'liking' a song
-       * Removing a specific song
-       * Examining all Songs (a String return or void print makes sense here)
-       * Examining a sublist of all liked songs
-       * Determining the total duration of all songs
-       * Removing all unliked songs from the playlist (careful with this one!)
-       */
+    public void removeSong(int position) {
+        int loc = position - 1;
+        if (loc >= 0 && loc < songs.size()) {
+            songs.remove(loc);
+        } else {
+            System.out.println("Cannot remove ts song: position " + position + " out of range. Lock in.");
+        }
+    }
+
+    public void printSongs() {
+        for (int i = 0; i < songs.size(); i++) {
+            Song s = songs.get(i);
+            System.out.println(s.toString());
+        }
+    }
+
+    public void printLikedSongs() {
+        for (int i = 0; i < songs.size(); i++) {
+            Song s = songs.get(i);
+            if (s.isLiked()) {
+                System.out.println(s.toString());
+            }
+        }
+    }
+
+    public String getTotalDurationString() {
+        int totalSeconds = 0;
+        for (int i = 0; i < songs.size(); i++) {
+            totalSeconds = totalSeconds + songs.get(i).getDurationSeconds();
+        }
+
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        if (seconds < 10) {
+            return minutes + ":0" + seconds;
+        } else {
+            return minutes + ":" + seconds;
+        } //this took a while to figure out
+    }
+
+    
+    public void removeUnlikedSongs() {
+        for (int i = songs.size() - 1; i >= 0; i--) {
+            if (!songs.get(i).isLiked()) {
+                songs.remove(i); //this also took so long
+            }
+        }
+    }
+
+    public int size() {
+        return songs.size();
+    }
 }
